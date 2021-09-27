a3cad26ede4fc22c89108e829864050d1cad9867
Krab <krabcode@gmail.com>
2021-09-27 11:13:59 +0200

    displaying input defined embeds

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
-var Discord = require('discord.io');
+var DiscordIO = require('discord.io');
+const DiscordJS = require('discord.js')
 var logger = require('winston');
 var auth = require('./auth.json');
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -8,76 +10,30 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 // Initialize Discord Bot
-var bot = new Discord.Client({
+var bot = new DiscordIO.Client({
     token: auth.token,
     autorun: true
 });
 bot.on('ready', function (evt) {
     logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info('Logged in as: ' + bot.username + ' - (' + bot.id + ')');
 });
 
 function getRandomChallengeTopic() {
     let topics = [
         "water", "trees", "text", "hue", "circle", "wave", "map", "organic", "noise", "retro", "pixelated", "fractal" // krab
-        ,"rhythm","sharp","red","grid","duplicate","shrinking","calm","speed" // pseudo_me
+        ,"rhythm","sharp","red","grid","duplicate","shrinking","calm","speed", // pseudo_me
+        "mathematical", "proc-gen", "2D", "3D", // Pyro
+        "screentone / hatching", "glitch", "chase", "portal", "thrust", "update", "rolling", // nking
+        "orbit", "launch", "fly", "paper plane", "garden", "rain", "waterfall", "corridor" // CaveHex
     ];
     let randomIndex = Math.floor(Math.random() * topics.length);
     return topics[randomIndex];
 }
 
 // TODO :
-// submission input for rich embed output via either text json or slash command
+// submission input for rich embed output via either text, json or slash command
 // count submission embeds for a given topic in the last n days and display reaction counts
-const exampleEmbed = {
-    color: 0x0099ff,
-    title: 'Some title',
-    url: 'https://discord.js.org',
-    author: {
-        name: 'Some name',
-        icon_url: 'https://i.imgur.com/AfFp7pu.png',
-        url: 'https://discord.js.org',
-    },
-    description: 'Some description here',
-    thumbnail: {
-        url: 'https://i.imgur.com/AfFp7pu.png',
-    },
-    fields: [
-        {
-            name: 'Regular field title',
-            value: 'Some value here',
-        },
-        {
-            name: '\u200b',
-            value: '\u200b',
-            inline: false,
-        },
-        {
-            name: 'Inline field title',
-            value: 'Some value here',
-            inline: true,
-        },
-        {
-            name: 'Inline field title',
-            value: 'Some value here',
-            inline: true,
-        },
-        {
-            name: 'Inline field title',
-            value: 'Some value here',
-            inline: true,
-        },
-    ],
-    image: {
-        url: 'https://i.imgur.com/AfFp7pu.png',
-    },
-    timestamp: new Date(),
-    footer: {
-        text: 'Some footer text here',
-        icon_url: 'https://i.imgur.com/AfFp7pu.png',
-    },
-};
 
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
@@ -85,21 +41,30 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     if (message.substring(0, 1) === '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-
-        args = args.splice(1);
         switch (cmd) {
             // !ping
             case 'random': {
                 bot.sendMessage({
                     to: channelID,
-                    message: 'This week\'s challenge is: ' + getRandomChallengeTopic()
+                    message: 'The next challenge prompt is: ' + getRandomChallengeTopic()
                 });
                 break;
             }
             case 'submit':
+                let embed = new DiscordJS.MessageEmbed();
+                console.log(message.substring(8));
+                let msgParts = message.substring(8).split(' ');
+                console.log(msgParts);
+                embed.setTitle(msgParts[0]);
+                embed.setThumbnail(msgParts[1])
+                embed.setDescription("hello");
+                embed.setImage(msgParts[1]);
+                embed.setURL(msgParts[1]);
+                // TODO fix animated imgur embed or abandon embeds
+                // test with "!submit Krab https://i.imgur.com/OwXPbQZ.mp4"
                 bot.sendMessage({
                     to: channelID,
-                    embed: exampleEmbed
+                    embed: embed.toJSON()
                 });
                 break;
             case 'count': {
@@ -107,11 +72,13 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 let scores = {};
                 bot.getMessages({channelID,}, function (error, messages) {
                     messages.forEach(msg => {
+                        console.dir(msg);
                         if (msg.content.substring(0, 7) === "!submit") {
                             console.dir(msg);
                             if (msg.reactions !== undefined) {
                                 msg.reactions.forEach(react => {
                                     if(msg.reactions.emoji !== null){
+                                        // TODO count votes for each user, then sort users by vote count and display
                                         console.log();
                                         console.log("react.emoji: ");
                                         console.dir(react.emoji);
