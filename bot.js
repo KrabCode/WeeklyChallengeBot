742ef7c892d1cec260ba675b74944b8490bb90df
Krab <krabcode@gmail.com>
2021-09-27 11:19:04 +0200

    tiny refactor

--- bot.js
+++ bot.js
@@ -21,8 +21,8 @@ bot.on('ready', function (evt) {
 
 function getRandomChallengeTopic() {
     let topics = [
-        "water", "trees", "text", "hue", "circle", "wave", "map", "organic", "noise", "retro", "pixelated", "fractal" // krab
-        ,"rhythm","sharp","red","grid","duplicate","shrinking","calm","speed", // pseudo_me
+        "water", "trees", "text", "hue", "circle", "wave", "map", "organic", "noise", "retro", "pixelated", "fractal", // Krab
+        "rhythm","sharp","red","grid","duplicate","shrinking","calm","speed", // pseudo_me
         "mathematical", "proc-gen", "2D", "3D", // Pyro
         "screentone / hatching", "glitch", "chase", "portal", "thrust", "update", "rolling", // nking
         "orbit", "launch", "fly", "paper plane", "garden", "rain", "waterfall", "corridor" // CaveHex
